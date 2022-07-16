55e2777a642eebb755f83e6ad5aee38caa1e9a12
HamedBagheri1992 <bagheri.h224@gmail.com>
2022-07-16 20:22:50 +0430

    Init

---
+++ src/services/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
